ba5c8d13c1fd925f52eb393d7bb6a2eff15f7e3b
VietNguyenQuoc <viet.nguyen@2359media.com>
2020-03-20 11:31:37 +0700

    add route get flight from id

--- index.js
+++ index.js
@@ -71,6 +71,18 @@ app.get("/flight/subscribe", async (req, res) => {
   });
 });
 
+app.get("/flight/subscribe/:id", async (req, res) => {
+  const { email } = req.query;
+  const flight = await client.hget(`users:${email}`, req.params.id);
+
+  if (!flight)
+    return res
+      .status(404)
+      .json({ success: false, message: "Flight not found" });
+
+  return res.status(200).json({ success: true, data: flight });
+});
+
 app.post("/flight/unsubscribe", async (req, res) => {
   try {
     const { email, id } = req.body;
